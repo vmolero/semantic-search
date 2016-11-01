d769c594f165151934fb9308678ea0fa0bf154f5
Victor Molero <victor.molero@gmail.com>
2016-11-01 01:49:57 +0100

    readme and install scripts

--- run.bat
+++ run.bat
@@ -2,3 +2,5 @@ call bower install
 call composer install
 call php app/console assets:install --symlink web
 call php app/console assetic:dump
+start php app/console server:run
+start firefox http://localhost:8000/rate/new
--- run.sh
+++ run.sh
@@ -4,3 +4,5 @@ bower install
 composer install
 php app/console assets:install --symlink web
 php app/console assetic:dump
+php app/console server:run &
+firefox http://localhost:8000/rate/new &
